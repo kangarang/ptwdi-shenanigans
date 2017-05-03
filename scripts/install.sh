34a19d52a48e4b5329c168a3ff3da01508e4ebb7
Isaac Kang <isaacjkang@gmail.com>
2017-05-02 22:27:52 -0400

    add install.sh

---
+++ scripts/install.sh
@@ -0,0 +1,21 @@
+echo "Hello!"
+sleep 2
+
+echo "This script will add some stuff to your .bash_profile"
+sleep 2
+
+echo "Like COLORS!"
+sleep 1
+
+echo "And some spacing"
+sleep 2
+
+echo "Let's begin by going home"
+sleep 4
+
+cd ~
+echo "Yay!"
+
+# curl -o- https://raw.githubusercontent.com/kangarang/ptwdi-shenanigans/master/scripts/.bash_profile >> ./.bash_profile
+
+# source ~/.bash_profile
--- scripts/readme.md
+++ scripts/readme.md
@@ -1,3 +1,9 @@
+# Scripts
+
 Both dotfiles belong in `~` directory.
 
 If these files already exist in your home directory, inspect the files in a text editor, read the comments, and change accordingly at your will.
+
+### Append .bash_profile from command line
+
+    curl -o- https://raw.githubusercontent.com/kangarang/ptwdi-shenanigans/master/scripts/install.sh | bash
