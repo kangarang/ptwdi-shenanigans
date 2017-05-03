8d8dedcbc5b79fad41143016a53d80a6c8af40a4
Isaac Kang <isaacjkang@gmail.com>
2017-05-02 22:49:23 -0400

    testing spacing

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,22 +1,23 @@
-echo 
+echo ""
 echo "Hello!"
 sleep 1
-echo 
+echo ""
 echo "This script will add some helpful stuff to your ~/.bash_profile"
 sleep 2
-echo 
+echo ""
 echo "I'm gonna add aliases, colors, spacing, and git information to your command line"
 sleep 3
-echo 
+echo ""
 echo "Check out the readme for more details"
 sleep 1
-echo 
+echo ""
 echo "Ok, let's start!"
 sleep 1
-echo 
+echo ""
 curl -o- https://raw.githubusercontent.com/kangarang/ptwdi-shenanigans/master/scripts/.bash_profile >> ~/.kang
 
 source ~/.bash_profile
-echo 
-echo 
+
+\n\n
+
 echo "All done! Enjoy your new shell!"
